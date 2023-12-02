e4ce2d57a730018e645017c22bc6eac0a0e68ffd
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-12-02 10:47:20 -0800

    Minesweeper working so far

---
+++ Assets/Scripts/SweeperManager.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SweeperManager : MonoBehaviour
+{
+    [SerializeField] private Transform tilePrefab, gameHolder;
+    private int width, height, numMines;
+
+    private List<Tile> tiles = new();
+
+    private readonly float tileSize = 0.32f;
+
+    private void Start()
+    {
+        CreateGameBoard(9, 9, 10); //Easy
+        // CreateGameBoard(16, 16, 40); //Intermediate
+        //CreateGameBoard(30,16,99); //Expert
+        ResetGameState();
+    }
+    private void CreateGameBoard(int width, int height, int numMines)
+    {
+        //save the game parameters we're using.
+        this.width = width;
+        this.height = height;
+        this.numMines = numMines;
+
+        //create the array of tiles.
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                //Position the tile in the correct place (centered).
+                Transform tileTransform = Instantiate(tilePrefab);
+                tileTransform.parent = gameHolder;
+                float xIndex = col - ((width -1)/2.0f);
+                float yIndex = row - ((width -1)/2.0f);
+                tileTransform.localPosition = new Vector2(xIndex * tileSize, yIndex * tileSize);
+
+                //Keep a reference to the til for setting up the game
+                Tile tile = tileTransform.GetComponent<Tile>();
+                tiles.Add(tile);
+
+            }
+        }
+    }
+
+    private void ResetGameState()
+    {
+        //Randomly shuffle the tile posiiton to get indices for mine positions.
+        int[] minePositions = (int[])Enumerable.Range(0, tiles.Count).OrderBy(x => Random.Range(0.0f, 1.0f)).ToArray(); 
+
+        for (int i = 0; i < numMines; i++)
+        {
+            int pos = minePositions[i];
+            tiles[pos].isMine = true;
+        }
+        for (int i = 0; i < tiles.Count; ++i)
+        {
+            tiles[i].mineCount = HowManyMines(i);
+        }
+    }
+
+    //Given a location work out how many mines are surrounding it
+    private int HowManyMines(int location)
+    {
+        int count = 0;
+        foreach (int pos in GetNeighbours(location))
+        {
+            if (tiles[pos].isMine)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private List<int> GetNeighbours(int pos)
+    {
+        List<int> neighbours = new();
+        int row = pos / width;
+        int col = pos % width;
+
+        //(0,0) is bottom left
+        if (row < (height - 1))
+        {
+            neighbours.Add(pos + width); //North
+            if (col > 0)
+            {
+                neighbours.Add(pos + width - 1); //North-West
+            }
+            if (col < (width - 1))
+            {
+                neighbours.Add(pos + width + 1); //North-East
+            }
+        }
+        if (col > 0)
+        {
+            neighbours.Add(pos - 1); //West
+        }
+        if (col < (width - 1))
+        {
+            neighbours.Add(pos + 1); //East
+        }
+
+        if (row > 0)
+        {
+            neighbours.Add(pos - width); //South
+            if (col > 0)
+            {
+                neighbours.Add(pos - width - 1); //South-West
+            }
+            if (col < (width - 1))
+            {
+                neighbours.Add(pos - width + 1); //South-East
+            }
+        }
+
+
+        return neighbours;
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,24 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField]private Color baseColor, OffsetColor;
-    [SerializeField] private SpriteRenderer _renderer;
+    //[SerializeField]private Color baseColor, OffsetColor;
+    //[SerializeField] private SpriteRenderer _renderer;
 
-    [SerializeField] private GameObject _highlight;
+    //[SerializeField] private GameObject _highlight;
 
-    public void Init(bool isOffset)
+    //public void Init(bool isOffset)
+    //{
+    //    _renderer.color = isOffset ? OffsetColor : baseColor;
+    //}
+    //private void OnMouseEnter()
+    //{
+    //    _highlight.SetActive(true);
+    //}
+    //private void OnMouseExit()
+    //{
+    //    _highlight.SetActive(false); 
+    //}
+
+    [Header("Tile Sprites")]
+    [SerializeField] private Sprite unclickedTile, flaggedTile, mineTile, mineWrongTile, mineHitTile;
+    [SerializeField] private List<Sprite> clickedTiles;
+
+    private SpriteRenderer spriteRenderer;
+    public bool flagged = false;
+    public bool active = true;
+    public bool isMine = false;
+    public int mineCount = 0;
+
+    private void Awake()
     {
-        _renderer.color = isOffset ? OffsetColor : baseColor;
+        //This Should exist due to the RequireComponent Helper
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-    private void OnMouseEnter()
+
+    private void OnMouseOver()
     {
-        _highlight.SetActive(true);
+        if (active)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //LeftClick reveals Tile
+                ClickedTile();
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                //Place a flag or remove a flag
+                flagged = !flagged;
+                if (flagged)
+                {
+                    spriteRenderer.sprite = flaggedTile;
+                }
+                else
+                {
+                    spriteRenderer.sprite = unclickedTile;
+                }
+
+            }
+        }
     }
-    private void OnMouseExit()
+    public void ClickedTile()
     {
-        _highlight.SetActive(false); 
+        if (active && !flagged)
+        {
+            active = false;
+            if (isMine)
+            {
+                //Uh Oh
+                spriteRenderer.sprite = mineHitTile;
+            }
+            else
+            {
+                //tile is safe
+                spriteRenderer.sprite = clickedTiles[mineCount];
+            }
+        }
     }
 }
